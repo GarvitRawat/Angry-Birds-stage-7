8c39b783700cc971ec51fc1492e1087d8d701f84
GarvitRawat <garvitrawat187@gmail.com>
2021-02-14 18:49:55 +0530

    Amgry Birds Stage 67

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ var bird, slingshot;
 var gameState = "onSling";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getTime()
 }
 
 function setup(){
@@ -45,7 +45,9 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    if (backgroundImg){
+        background(backgroundImg);
+    }
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
@@ -67,6 +69,8 @@ function draw(){
     platform.display();
     //log6.display();
     slingshot.display();    
+
+
 }
 
 function mouseDragged(){
@@ -85,4 +89,20 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+async function getTime(){
+    var responds = await fetch("http://worldtimeapi.org/api/timezone/Asia/Calcutta")
+    var respondsJson = await responds.json()
+    var dateTime = respondsJson.datetime
+  //  console.log(dateTime)
+    var hour = dateTime.slice(11, 13)
+    console.log(hour)
+
+    if (hour > 17 && hour < 6){
+        bg = "sprites/bg.png"
+    }else{
+        bg = "sprites/bg2.jpg"
+    }
+    backgroundImg = loadImage(bg)
 }
